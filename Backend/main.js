e9027380e7164d496d2502823615ca0fc0c674b3
Yassin Bendella <yassin.bendella@student.ehb.be>
2023-08-04 23:46:02 +0100

    calender update

--- Backend/main.js
+++ Backend/main.js
@@ -1,29 +1,17 @@
 import express from "express";
-import { Database } from './database/database.js'
+import {calendarRoutes} from './routes/calendar.js'
+import cors from 'cors'
+import bodyParser from "body-parser";
+
 const port = 3000
+
 const url = `mongodb+srv://yassin:yassin123@cluster0.1ttwg.mongodb.net/`
-
-
 const app = express()
-const database = new Database(url, "smartmirror")
+app.use(cors())
+app.use(bodyParser())
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
 })
 
-app.get("/", async (req,res) => {
-    let today = new Date()
-    today.setHours(0)
-    today.setMinutes(0)
-    today.setSeconds(0)
-
-    
-    let midnight = new Date()
-    midnight.setHours(23)
-    midnight.setMinutes(59)
-    midnight.setSeconds(59)
-
-    const items = await database.getCalendarItemsBetweenDates(today,midnight)
-    console.log(items)
-    res.send(items)
-})
+app.use('/calendar', calendarRoutes);
---
+++ Backend/routes/calendar.js
@@ -0,0 +1,41 @@
+import express from 'express'
+import { Database } from '../database/database.js'
+
+const calendarRoutes = express.Router();
+const url = `mongodb+srv://yassin:yassin123@cluster0.1ttwg.mongodb.net/`
+const database = new Database(url, "smartmirror")
+
+calendarRoutes.get('/:offset', async (req,res) => {
+    let offset = req.params.offset || 0;
+    offset = parseInt(offset);
+    if (isNaN(offset)) offset = 0;
+
+    let today = new Date();
+    today.setHours(0);
+    today.setMinutes(0);
+    today.setSeconds(0);
+    today.setDate(today.getDate() + offset)
+    
+    
+    let midnight = new Date();
+    midnight.setHours(23);
+    midnight.setMinutes(59);
+    midnight.setSeconds(59);
+    midnight.setDate(midnight.getDate() + offset)
+    
+    console.log(today)
+    console.log(midnight)
+
+    const items = await database.getCalendarItemsBetweenDates(today,midnight);
+    res.send(items);
+});
+
+calendarRoutes.post('/add', async (req, res) => {
+    body = req.body
+    
+})
+
+export {
+    calendarRoutes
+}
+
